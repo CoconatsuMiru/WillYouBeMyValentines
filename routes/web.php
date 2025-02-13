517665a607b66f4d83f303afc0806f3382a203a1
ArAr <hhfrostie@gmail.com>
2025-02-13 23:52:31 +0800

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ask.valentine');
 });
 
 Route::get('/valentine', 'App\Http\Controllers\ValentineController@index');
